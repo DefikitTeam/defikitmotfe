e02c3e7cbd9e57207c0cec2315cc1f80e46b0ab0
DESKTOP-CTOISJ3\kiennv <kiennv.contact@gmail.com>
2025-03-16 20:59:23 +0700

    fix something

--- src/views/pool-detail/deposit-lottery-button.tsx
+++ src/views/pool-detail/deposit-lottery-button.tsx
@@ -12,7 +12,7 @@ import { EActionStatus } from '@/src/stores/type';
 import { Button, notification, Spin } from 'antd';
 import BigNumber from 'bignumber.js';
 import { Loader } from 'lucide-react';
-import { useTranslations } from 'next-intl';s
+import { useTranslations } from 'next-intl';
 import { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
 
