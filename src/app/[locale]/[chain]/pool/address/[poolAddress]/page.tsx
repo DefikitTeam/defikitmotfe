d4c1d51d89289454ef8b499ae17005619a95c357
DESKTOP-CTOISJ3\kiennv <kiennv.contact@gmail.com>
2025-05-28 10:12:48 +0700

    feat: Opengraph token detail when share via social

--- src/app/[locale]/[chain]/pool/address/[poolAddress]/page.tsx
+++ src/app/[locale]/[chain]/pool/address/[poolAddress]/page.tsx
@@ -1,6 +1,72 @@
 /* eslint-disable */
 
+import { ConfigService } from '@/src/config/services/config-service';
+import servicePool from '@/src/services/external-services/backend-server/pool';
+import { updateMetaDataWorker } from '@/src/stores/pool/common';
 import PoolDetail from '@/src/views/pool-detail';
+import type { Metadata, ResolvingMetadata } from 'next';;
+const configService = ConfigService.getInstance();
+type Props = {
+    params: { locale: string; chain: string; poolAddress: string; }
+    searchParams: { [key: string]: string | undefined }
+}
+
+
+
+
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+    const tokenAddress = params.poolAddress;
+    // const chain = params.chain;
+    // const locale = params.locale;
+    // console.log('chain line 20-----', chain
+
+    // )
+    // console.log('locale line 21-----', locale)
+    // console.log('tokenAddress line 19-----', tokenAddress)
+    let tokenData: any = null
+    let metadata: any = null
+    try {
+        const [poolDetails, metadataUpdateResult] = await Promise.all([
+            servicePool.getDetailPoolDataFromServer(configService.getDefaultChain().toString(), tokenAddress),
+            updateMetaDataWorker(tokenAddress,configService.getDefaultChain().toString())
+        ]);
+        tokenData = poolDetails;
+        metadata = metadataUpdateResult
+
+        // console.log('Fetched poolDetails for metadata:', tokenData);
+        // console.log('Result of updateMetaDataWorker:', metadataUpdateResult);
+    } catch (error) {
+        console.error('Error fetching data in generateMetadata:', error);
+    }
+
+    // optionally access and extend (rather than replace) parent metadata
+    const previousImages = (await parent).openGraph?.images || []
+
+    return {
+        openGraph: {
+            type: 'website',
+            title: tokenData.data?.name,
+            description: metadata?.description,
+            images: [
+                {
+                    url: metadata?.image || '',
+                    width: 1200,
+                    height: 630,
+                    alt: `${tokenData.data?.name}'s Logo`
+                },
+                ...previousImages
+            ]
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: tokenData.data?.name,
+            description: metadata?.description,
+            images: [metadata?.image || '']
+        }
+    }
+}
+
+
 
 export default PoolDetail;
 
