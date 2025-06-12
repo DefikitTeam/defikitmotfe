265a4cf150a9d89feb6fc9ee2dc724659af75465
DESKTOP-CTOISJ3\kiennv <kiennv.contact@gmail.com>
2025-06-12 11:13:05 +0700

    fix  build v1

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate", 
+            value: "no-cache, no-store, must-revalidate",
           },
         ],
       },
@@ -19,7 +19,7 @@ const nextConfig = {
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    
+
     config.module.rules.push(
       {
         test: /\.d\.ts$/,
@@ -39,7 +39,43 @@ const nextConfig = {
       test: /node_modules\/@metamask\/sdk\/.*\.(d\.ts|map)$/,
       loader: 'ignore-loader'
     });
-    
+
+    // Handle web workers properly
+    config.module.rules.push({
+      test: /\.worker\.(js|ts)$/,
+      use: {
+        loader: 'worker-loader',
+        options: {
+          name: 'static/[hash].worker.js',
+          publicPath: '/_next/'
+        }
+      }
+    });
+
+    // Configure Terser optimization to handle module syntax in workers
+    if (config.optimization && config.optimization.minimizer) {
+      config.optimization.minimizer.forEach((minimizer) => {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          if (!minimizer.options.exclude) {
+            minimizer.options.exclude = [];
+          }
+          // Exclude worker files and files with HeartbeatWorker in the name
+          minimizer.options.exclude.push(/HeartbeatWorker/);
+          minimizer.options.exclude.push(/\.worker\./);
+
+          // Update terser options to handle module syntax
+          if (!minimizer.options.terserOptions) {
+            minimizer.options.terserOptions = {};
+          }
+          if (!minimizer.options.terserOptions.parse) {
+            minimizer.options.terserOptions.parse = {};
+          }
+          minimizer.options.terserOptions.parse.ecma = 2020;
+          minimizer.options.terserOptions.module = true;
+        }
+      });
+    }
+
     return config;
   },
   // images: {
